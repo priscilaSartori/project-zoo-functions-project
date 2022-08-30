df2bf82b9c5762a1fb1c8d3bfa2e42ea1d24e465
Priscila <priscilasartori2@gmail.com>
2022-08-30 19:45:55 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+// A função countAnimals, caso não receba nenhum parâmetro, deve retornar todas as espécies e a quantidade de residentes de cada uma;
+// A função countAnimals, caso receba como parâmetro um objeto com a chave specie, deve retornar a quantidade de animais daquela espécie;
+// A função countAnimals, caso receba como parâmetro um objeto com as chaves specie e sex, deve retornar a quantidade de animais daquela espécie, no sexo selecionado.
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const { residents } = require('../data/zoo_data').species;
 
 function countAnimals(animal) {
-  // seu código aqui
+  let animais = {};
+  if (animal === undefined) {
+    species.map((specie) => animais[specie.name] = specie.residents.length)
+    return animais
+  } else if (animal.sex === "female" || animal.sex === "male") {
+    return species.find((specie) => specie.name.includes(animal.specie)).residents.filter((resident) => resident.sex === animal.sex).length
+  } else if (animal.sex === undefined) {
+    return species.find((specie) => specie.name.includes(animal.specie)).residents.length;
+  }
 }
+    
+console.log(countAnimals())
 
+      
 module.exports = countAnimals;
