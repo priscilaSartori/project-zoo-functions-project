d87d055745c84616bc7f09563470636e299a3ef3
Priscila <priscilasartori2@gmail.com>
2022-08-31 19:16:04 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,45 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+  let somaEntrada = {};
+  somaEntrada = { child: 0, adult: 0, senior: 0 };
+  entrants.filter((entrant) => {
+    if (entrant.age < 18) {
+      somaEntrada.child += 1;
+    } else if (entrant.age < 50) {
+      somaEntrada.adult += 1;
+    } else if (entrant.age >= 50) {
+      somaEntrada.senior += 1;
+    }
+  });
+  return somaEntrada;
+}
+
+function valor(entrants) {
+  let pago = 0;
+  entrants.filter((entrant) => {
+    if (entrant.age < 18) {
+      pago += data.prices.child;
+    } else if (entrant.age < 50) {
+      pago += data.prices.adult;
+    } else if (entrant.age >= 50) {
+      pago += data.prices.senior;
+    }
+  });
+  return Number(parseFloat(pago).toFixed(2));
 }
 
 function calculateEntry(entrants) {
+  let ingressos = 0;
+  if (!entrants) {
+    ingressos = 0;
+  } else if (Object.entries(entrants).length === 0) {
+    ingressos = 0;
+  } else {
+    const valorTotal = valor(entrants);
+    return valorTotal;
+  }
+  return ingressos;
 }
 
 module.exports = { calculateEntry, countEntrants };
