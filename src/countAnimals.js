cb2fbb2b7178bd0860854d3c110ffab58221d6e4
Priscila <priscilasartori2@gmail.com>
2022-09-03 17:59:20 -0300

    Requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,23 +1,22 @@
-// A função countAnimals, caso não receba nenhum parâmetro, deve retornar todas as espécies e a quantidade de residentes de cada uma;
-// A função countAnimals, caso receba como parâmetro um objeto com a chave specie, deve retornar a quantidade de animais daquela espécie;
-// A função countAnimals, caso receba como parâmetro um objeto com as chaves specie e sex, deve retornar a quantidade de animais daquela espécie, no sexo selecionado.
 const data = require('../data/zoo_data');
-const { species } = require('../data/zoo_data');
-const { residents } = require('../data/zoo_data').species;
 
-function countAnimals(animal) {
-  let animais = {};
-  if (animal === undefined) {
-    species.map((specie) => animais[specie.name] = specie.residents.length)
-    return animais
-  } else if (animal.sex === "female" || animal.sex === "male") {
-    return species.find((specie) => specie.name.includes(animal.specie)).residents.filter((resident) => resident.sex === animal.sex).length
-  } else if (animal.sex === undefined) {
-    return species.find((specie) => specie.name.includes(animal.specie)).residents.length;
+function contSex(animal) {
+  if (Object.values(animal).includes('female')) {
+    const specieName = data.species.find((specie) => specie.name.includes(animal.specie));
+    return specieName.residents.filter((resident) => resident.sex === 'female').length;
+  } if (Object.values(animal).includes('male')) {
+    const specieName = data.species.find((specie) => specie.name.includes(animal.specie));
+    return specieName.residents.filter((resident) => resident.sex === 'male').length;
   }
 }
-    
-console.log(countAnimals())
 
-      
+function countAnimals(animal) {
+  if (animal === undefined) {
+    return data.species.reduce((sum, specie) => ({
+      ...sum, [specie.name]: specie.residents.length }), {});
+  } if (Object.keys(animal).includes('sex')) {
+    return contSex(animal);
+  } return data.species.find((specie) => specie.name.includes(animal.specie)).residents.length;
+}
+
 module.exports = countAnimals;
