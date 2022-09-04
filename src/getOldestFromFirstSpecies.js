c4bfd64f828ab85d8bc02f29c73034d982fbcbf8
Priscila <priscilasartori2@gmail.com>
2022-09-03 23:52:00 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const primeiroAnimal = data.employees.find((employee) => employee.id === id);
+  const primeiroresp = primeiroAnimal.responsibleFor.find((employe) => employe);
+  const especie1 = data.species.find((specie) => specie.id === primeiroresp).residents;
+  const especie2 = especie1.map((espec) => Object.values(espec));
+  return especie2.reduce((acc, curr) => {
+    if (acc[2] > curr[2]) {
+      return acc;
+    } return curr;
+  });
 }
 
 module.exports = getOldestFromFirstSpecies;
