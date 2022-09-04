5ec34fa88dce1dfe52851e77867f1c100a97109b
Priscila <priscilasartori2@gmail.com>
2022-09-04 02:24:42 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -4,20 +4,20 @@ const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
 const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
 const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
 const managers = [stephanieId, olaId, burlId];
-const { species, employees, hours, prices} = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 // A função isManager deve retornar true se o ID passado for de uma pessoa gerente;
 // A função isManager deve retornar false se o ID passado não for de uma pessoa gerente;
 const isManager = (id) => managers.some((manager) => manager === id);
-console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'))
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 // A função getRelatedEmployees, caso o ID passado for da pessoa gerente, deve retornar um array contendo nome e sobrenome das pessoas colaboradoras pelas quais ela é responsável;
 function getRelatedEmployees(managerId) {
   if (isManager(managerId)) {
-    const manager2 = employees.filter((employee) => employee.managers.some((manager) => manager === managerId)
-    )
-    return manager2.map((manager) => `${manager.firstName} ${manager.lastName}`)
+    const manager2 = employees.filter((employee) => employee.managers
+      .some((manager) => manager === managerId));
+    return manager2.map((manager) => `${manager.firstName} ${manager.lastName}`);
   } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-  }
+}
 
 module.exports = { isManager, getRelatedEmployees };
